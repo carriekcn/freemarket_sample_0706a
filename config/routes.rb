b4e2b237cc181b6caab0ff721a4d4f83bb1c6019
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-07-15 10:12:09 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#test'
+  #root 'application#test'
+  root to: 'top#index'
+  resources :top, only: [:index]
 end
