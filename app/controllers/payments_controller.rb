5b80a911e61b90b7eb0fa4d33916cd1e2328c073
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-07-31 14:03:43 +0900

    Update payment_controller.rb

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,18 @@
 class PaymentsController < ApplicationController
+  #before_action :authenticate_user!
+
+  def add_card
+    #form表示
+  end
+
+  def create
+    #model名.create(create_params)
+    #recirect_to controller: :mypageのコントローラとアクション
+  end
+
+  private
+  def create_params
+    #params.require(:)
+  end
+
 end
