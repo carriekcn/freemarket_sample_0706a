635bd1b88d462c51590b02e1be8ba30c50d60a25
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-08-01 20:57:48 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'cards/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'top#index'
-  resources :top, only: [:index]
-  #resources :top do
-  #resources :payment, only: [:index, :create]
-  #end
 
+  #get 'cards/index'
+
+  root to: 'top#index'
+  resources :top, only: [:index] do
+    #resources :mypage .... do
+    resources :cards, only: [:index, :new, :create]
+  end
 
 end
