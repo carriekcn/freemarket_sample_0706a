97d735768e15060d1350b7bce0ab4ddc6cb95806
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-08-04 18:49:06 +0900

    Update migration file

--- db/migrate/20190804094710_create_comments.rb
+++ db/migrate/20190804094710_create_comments.rb
@@ -1,7 +1,11 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-
+      t.references :user, foreign_key: true
+      t.references :items, foreign_key: true
+      t.text :body, null: false
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
       t.timestamps
     end
   end
