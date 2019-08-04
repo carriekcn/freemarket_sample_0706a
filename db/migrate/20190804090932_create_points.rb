ad0f6531d08a62ff83fdbf06677c16ee38db2185
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-08-04 18:38:29 +0900

    Delete migratoin file due to table existence

--- db/migrate/20190804090932_create_points.rb
+++
@@ -1,9 +0,0 @@
-class CreatePoints < ActiveRecord::Migration[5.2]
-  def change
-    create_table :points do |t|
-      t.integer :tickets_point
-      t.integer :merupay_point
-      t.timestamps
-    end
-  end
-end
