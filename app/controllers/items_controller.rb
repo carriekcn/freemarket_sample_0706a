a8402097c21fa64acf67d8f8adae193d50394a03
katsuki-knn <xj_katsuki@yahoo.co.jp>
2019-08-12 21:54:25 +0900

    商品出品実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,28 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+  layout "compact", only: [:new]
 
   def show
   end
 
   def new
-    render layout:  "compact"
+    @item = Item.new
+    @item_image = @item.item_images.build
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      params[:item_images]['image'].each do |i|
+        @item_image = @item.item_images.create!(image: i, item_id: @item.id)
+      end
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :state, :shipping_charges, :shipping_method, :shipping_source_area, :days_ship, :price, item_images_attributes: [:image]).merge(user_id: current_user.id)
   end
 end
