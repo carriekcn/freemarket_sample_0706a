f4112ae3c51ef243fb9d18fb58d03d1b6fb4da50
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-08-12 18:21:18 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,5 @@ class Item < ApplicationRecord
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  belongs_to :item_image
+  has_many :item_images
 end
