5cda37431ef01db72807b7cbbdb8a3b8f45fcbc2
KuriY1213 <arumiruk.ikuy@gmail.com>
2019-08-19 15:13:23 +0900

    sidemenu_comit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'top#index'
+  resources :users, only: [:show]
   resources :top, only: [:index]
   resources :items, only: [:new, :show]
   resources :purchases, only: [:index]
