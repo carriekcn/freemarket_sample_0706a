3b4c42440b77da1d3a47e9c88b83bea6a7a18213
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-08-19 23:09:40 +0900

    Update controller and view

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,10 @@
 class TopController < ApplicationController
   def index
 
+    items = Item.where(category_id: 1).limit(4)
+    #@img = ItemImage.includes(:item).group(:item_id).where(item_id: items.ids)
+    @img = ItemImage.includes(:item).group(:item_id).where(item_id: items.ids).order('created_at DESC')
+
 
   end
 end
