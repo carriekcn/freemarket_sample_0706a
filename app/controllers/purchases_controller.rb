d4a0fe8c3b4be60aec0764774edcf32c357706a9
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-08-19 14:41:57 +0900

    Updated

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,4 +2,13 @@ class PurchasesController < ApplicationController
   def index
     render layout:  "compact"
   end
+
+  def update
+
+  end
+
+  private
+  def purchase_params
+    params.require(:item).permit()
+  end
 end
