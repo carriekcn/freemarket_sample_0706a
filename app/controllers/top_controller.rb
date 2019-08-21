dd7fc1f923088beb275f61d22e3d536426ede2cf
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-08-21 14:52:14 +0900

    Updated index.html.erb by editing men category

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,5 +2,9 @@ class TopController < ApplicationController
   def index
     items = Item.where(category_id: 1).limit(4)
     @img = ItemImage.includes(:item).group(:item_id).where(item_id: items.ids).order('created_at DESC')
+
+    mens = Item.where(category_id: 2).limit(4)
+    @men = ItemImage.includes(:item).group(:item_id).where(item_id: mens.ids).order('created_at DESC')
+
   end
 end
