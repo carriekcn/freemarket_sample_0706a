104a6d67db44adae49d15151122dfd7b58ebef57
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-08-26 16:19:24 +0900

    Modified top_controller and index.html.erb

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,24 +1,17 @@
 class TopController < ApplicationController
+  
   def index
+    @wmen = searchItemImage(1)
+    @men = searchItemImage(2)
+    @kids = searchItemImage(3)
+    @chans = searchItemImage(14)
+    @louvs = searchItemImage(15)
+    @nikes = searchItemImage(16)
+  end
 
-    wmen = Item.where(category_id: 1).limit(4)
-    @wmen = ItemImage.includes(:item).group(:item_id).where(item_id: wmen.ids).order('created_at DESC')
-
-    mens = Item.where(category_id: 2).limit(4)
-    @men = ItemImage.includes(:item).group(:item_id).where(item_id: mens.ids).order('created_at DESC')
-
-    kid = Item.where(category_id: 3).limit(4)
-    @kids = ItemImage.includes(:item).group(:item_id).where(item_id: kid.ids).order('created_at DESC')
-
-    chan = Item.where(category_id: 14).limit(4)
-    @chans = ItemImage.includes(:item).group(:item_id).where(item_id: chan.ids).order('created_at DESC')
-
-    louv = Item.where(category_id: 15).limit(4)
-    @louvs = ItemImage.includes(:item).group(:item_id).where(item_id: louv.ids).order('created_at DESC')
-
-    nike = Item.where(category_id: 16).limit(4)
-    @nikes = ItemImage.includes(:item).group(:item_id).where(item_id: nike.ids).order('created_at DESC')
-
+  def searchItemImage (category_id)
+    cat_id = Item.where(category_id: category_id).limit(4)
+    ItemImage.includes(:item).group(:item_id).where(item_id: cat_id.ids).order('created_at DESC')
   end
 
 end
