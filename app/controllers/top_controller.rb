8d9be90d293246ea65a544daeeb7fd6bffc896b8
katsuki-knn <xj_katsuki@yahoo.co.jp>
2019-08-29 19:40:44 +0900

    Update top_controller.rb

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -9,8 +9,8 @@ class TopController < ApplicationController
   end
 
   def searchItemImage (category_id)
-    items = Item.where(category_id: category_id).limit(4)
-    ItemImage.includes(:item).group(:item_id).where(item_id: items.ids).order('created_at DESC')
+    items = Item.where(category_id: category_id)
+    ItemImage.includes(:item).group(:item_id).where(item_id: items.ids).order('created_at DESC').limit(4)
   end
 
 end
