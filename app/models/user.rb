a43a5aa7fc2e16e8951c39e6f1dfd964d3bf051b
katsuki-knn <xj_katsuki@yahoo.co.jp>
2019-08-31 01:33:24 +0900

    userモデルの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,46 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   # アソシエーション
   has_many :items
+  has_many :sns_credentials, dependent: :destroy
   
   # バリデーション
   validates :nickname,                presence: true, length: {maximum: 20}
   validates :email,                   presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
   validates :password,                presence: true, length: {minimum: 6}
-  validates :password_confirmation,   presence: true, length: {minimum: 6}
+  # validates :password_confirmation,   presence: true, length: {minimum: 6}
 
+  def self.from_auth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    if snscredential.present?
+      user = User.where(id: snscredential.user_id).first
+    else
+      user = User.where(email: auth.info.email).first
+      if user.present?
+        SnsCredential.create(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+          )
+      else
+        user = User.create(
+          nickname: auth.info.name,
+          email:    auth.info.email,
+          password: Devise.friendly_token[0, 20],
+          )
+        SnsCredential.create(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+          )
+      end
+    end
+    return user
+  end
 end
