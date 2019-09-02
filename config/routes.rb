14e7af173141474194036735a2bb127dfb178c6a
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-09-02 22:06:49 +0900

    Remove edit from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   root to: 'top#index'
   resources :users, only: [:show]
   resources :top, only: [:index]
-  resources :items, only: [:new, :create, :show, :edit]
+  resources :items, only: [:new, :create, :show]
   resources :purchases, only: [:index]
   resources :users, only: [:index]
   resources :cards, only: [:index, :new, :create]
