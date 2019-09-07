4d816db8bb1e79351c08f9acb2048f8874c9cb6f
ryo <ryosakoi21@icloud.com>
2019-09-07 22:43:12 +0900

    商品削除(サーバーサイド)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.user_id == current_user.id
+      @item.destroy
+    redirect_to ("/")
+    end
+  end
+
   private
 
   def item_params
