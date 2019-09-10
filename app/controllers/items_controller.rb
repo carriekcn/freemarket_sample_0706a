e11a6051a418cc8a421b72f792024b9306e1f19d
katsuki-knn <xj_katsuki@yahoo.co.jp>
2019-09-10 20:54:53 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :confirmation, :confirmed]
   layout "compact", only: [:new, :edit]
 
   def new
